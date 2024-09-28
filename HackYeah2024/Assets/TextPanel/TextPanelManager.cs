f2b0c79c863646681b73b0c68d6b51d465c8b3e4
Mikołaj Ptasznik <rugall1998@gmail.com>
2024-09-28 15:52:30 +0200

    asstes added, basic text panel

---
+++ HackYeah2024/Assets/TextPanel/TextPanelManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TextPanelManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text textPanel;
+    [SerializeField] private GameObject optionsPanel;
+    [SerializeField] private GameObject optionPrefab;
+    
+}
